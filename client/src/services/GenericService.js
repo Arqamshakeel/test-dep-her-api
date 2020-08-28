9bf7ddf0a4856d371bb548f95312ca741aed90ef
arqamshakeel <arqam.android@gmail.com>
2020-08-28 18:42:01 +0500

    Update GenericService.js

--- client/src/services/GenericService.js
+++ client/src/services/GenericService.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 // axios.defaults.baseURL = "https://test-express-arqam.herokuapp.com/api/";
-axios.defaults.baseURL = "https://test-familymart.herokuapp.com/api";
+//axios.defaults.baseURL = "https://test-familymart.herokuapp.com/api";
+axios.defaults.baseURL =
+  "http://ec2-18-219-5-52.us-east-2.compute.amazonaws.com:8080";
 // axios.defaults.baseURL =
 //   "http://ec2-18-221-158-145.us-east-2.compute.amazonaws.com:5000/api/";
 // //amazon working
